b9e6e7332ba29f32d03533c71ffa664f61621b69
Rafael Rodriguez Viau <rodriguezviaurafael@gmail.com>
2022-05-11 23:30:21 -0300

    Bug fixing, added funtionality to update and delete buttons on home

--- src/public/app.js
+++ src/public/app.js
@@ -3,8 +3,7 @@ $(function(){
     const URI = '/api/operations';
     
 var operationsarray =[];
-let total=0;
-  // GET OPERATIONS
+  // GET LAST 10 OPERATIONS
   $('#refreshOperations').on('click', () => {
     $.ajax({
       url: URI,
@@ -14,22 +13,27 @@ let total=0;
     }
     });
   });
-//LOAD TABLE
-function calculartotal(){
-  operationsarray.forEach(operationsarray=>{
-    if (operationsarray.Tipo == "Ingreso")
-      total += operationsarray.Monto;
-    else
-      total -= operationsarray.Monto;
 
-    
-  }) 
-  $('h3').text("$"+total)
-}
 function updateValues(max){
   loadtable(max);
   calculartotal();
 }
+
+//LOAD TABLE
+function calculartotal(){
+  var total = 0;
+  operationsarray.forEach(operationsarray=>{
+    if (operationsarray.Tipo == "Ingreso"){
+      total = total + operationsarray.Monto;
+    }
+    else{
+      total = total - operationsarray.Monto;
+    }
+  }) 
+  $('h3').text("$"+total)
+}
+
+
 function loadtable(max){
         let tbody = $('tbody');
         tbody.html('');
@@ -40,8 +44,8 @@ function loadtable(max){
             else
               bg = "p-3 mb-2 bg-danger text-white";
             tbody.append(`
-                <tr class = "${bg}">
-                    <td class="id">${operationsarray.Id}</td>
+                <tr class = "${bg} table-row">
+                    <td class="id table-id">${operationsarray.Id}</td>
                     <td><input type="text" class="concepto" value="${operationsarray.Concepto}"/></td>
                     <td><input type="text" class="monto" value="${operationsarray.Monto}"/></td>
                     <td><input type="text" class="fecha" value="${operationsarray.fecha.substring(0,10)}"/></td>
@@ -79,14 +83,14 @@ function loadtable(max){
 // UPDATE OPERATIONS
   $('table').on('click', '.update-button', function() {
     let row = $(this).closest('tr');
-    let id = row.find('.id').text();
+    let vid = row.find('.id').text();
     let vconcepto = row.find('.concepto').val();
     let vmonto = row.find('.monto').val();
     let vfecha = row.find('.fecha').val();
     let vtipo = row.find('.tipo').val();
 
     $.ajax({
-      url: `${URI}/${id}`,
+      url: `${URI}/${vid}`,
       method: 'PUT',
       data: {
         Concepto: vconcepto,
@@ -95,22 +99,34 @@ function loadtable(max){
         Tipo : vtipo
       },
       success: function(response) {
-        $('#refreshOperations').click();
-      }
+        operationsarray.forEach(operationsarray => {         
+          if(operationsarray.Id == vid){
+            operationsarray.Monto = parseFloat(vmonto);
+          }
+        })
+        updateValues(-10);
+      },
     });
   });
+
 //DELETE OPERATIONS
   $('table').on('click', '.delete-button', function() {
     let row = $(this).closest('tr');
-    let id = row.find('.id').text();
+    let vid = row.find('.id').text();
 
     $.ajax({
-      url: `${URI}/${id}`,
+      url: `${URI}/${vid}`,
       method: 'DELETE',
       success: function (response) {
-       $('#refreshOperations').click();
+        for (let i = operationsarray.length - 1; i >= 0; i--) {
+          if (operationsarray[i].Id == vid) {
+            operationsarray.splice(i, 1);
+          }
+        }
+      updateValues(-10);
       }
     });
   });
+  
   
 })
